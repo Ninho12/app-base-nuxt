63b4caed24671f53602ae24dc15cc67450b1f253
Ninho12 <joaopaulojpsp@gmail.com>
2022-12-25 19:49:55 -0300

    deleção dos arquivos

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,15 @@
 <template>
   <div>
-    <MenuPrincipal />
-
-    <Hero/>
-    <Pagina>
-      <h1>João Paulo</h1>
-    </Pagina>
-    <Beneficios />
-
-    <FooterPrincipal />
+    
   </div>
 </template>
 
 <script>
-import Beneficios from '../components/partes/beneficios.vue';
-import FooterPrincipal from '../components/partes/footer-principal.vue';
-import Hero from '../components/partes/hero.vue';
-import MenuPrincipal from '../components/partes/menu-principal.vue';
-import Pagina from '../components/partes/pagina.vue';
 
 
 export default {
     name: "IndexPage",
-    components: { MenuPrincipal, Hero, FooterPrincipal, Beneficios, Pagina }
+    
 }
 
 </script>
