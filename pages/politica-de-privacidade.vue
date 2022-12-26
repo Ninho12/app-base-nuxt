a360a72803d3ad43dff7bbd414520f0fd60e36fc
Ninho12 <joaopaulojpsp@gmail.com>
2022-12-25 21:17:20 -0300

    Adição de varios componentes vue

--- pages/politica-de-privacidade.vue
+++ pages/politica-de-privacidade.vue
@@ -1,22 +1,18 @@
 <template>
     
     <div>
-        <MenuPrincipal />
-
-
-        <FooterPrincipal />
+       
     </div>
 
 
 </template>
 
 <script>
-import FooterPrincipal from '../components/partes/footer-principal.vue';
-import MenuPrincipal from '../components/partes/menu-principal.vue';
+
 
 export default {
     name: "Politica",
-    components: { FooterPrincipal, MenuPrincipal }
+   
 }
 
 </script>
