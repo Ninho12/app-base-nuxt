835a27ec16a13e5193e530664fca33900ef923be
Ninho12 <joaopaulojpsp@gmail.com>
2022-12-25 21:48:21 -0300

    Atualização importante

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    
+
+    <Header/>
+    <BeneficiosSimples/>
+
   </div>
 </template>
 
 <script>
+import BeneficiosSimples from '../components/principal/beneficios-simples.vue';
+import Header from '../components/principal/header.vue';
+
 
 
 export default {
     name: "IndexPage",
-    
+    components: { Header, BeneficiosSimples }
 }
 
 </script>
